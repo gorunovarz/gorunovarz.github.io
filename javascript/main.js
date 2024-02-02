57db2eabec9d3acf88527d372b8eb73ad44295d2
gorunovarz <oleggorunov2002@mail.ru>
2024-02-02 13:00:29 +0300

    Compressed files
    Add new files
    
    Cool!

--- javascript/main.js
+++ javascript/main.js
@@ -48,7 +48,7 @@ const btnUp = {
 btnUp.addEventListener();
 
 function addWelcomeFiles() {
-    let links = ["https://fedorsarafanov.github.io/", "https://rf-slf-4x1.github.io", "https://korzhimanov.ru/", "https://vk.com/gorunovarz"];
+    let links = ["https://fedorsarafanov.github.io/", "https://rf-slf-4x1.github.io", "https://korzhimanov.ru/", ""];
     let welcomeFiles = document.getElementsByClassName("welcome-file");
     if (links.length != welcomeFiles.length)
         throw new Error("Links length not equals welcome files length");
